98f0c1b55fa5b186243107e0f9ce603dc778c582
lilei@shrbank.com <1198189741@qq.com>
2017-05-24 14:01:23 +0800

    调整文档排版

--- src/com/HelloWorld.java
+++ src/com/HelloWorld.java
@@ -3,10 +3,12 @@ package com;
 /**
  * Created by user on 2017/5/12.
  */
-public class HelloWorld {
-    /* 第一个Java程序
+
+/* 第一个Java程序
      * 它将打印字符串 Hello World
      */
+
+public class HelloWorld {
     public static void main(String []args) {
         System.out.println("Hello World"); // 打印 Hello World
     }
