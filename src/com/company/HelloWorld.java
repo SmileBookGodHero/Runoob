33b4ed2ba86ef344957843a43c6816321be5afa1
李磊 <1198189741@qq.com>
2020-09-15 19:06:11 +0800

    源码环境更新

--- src/com/company/HelloWorld.java
+++ src/com/company/HelloWorld.java
@@ -1,10 +1,12 @@
 package com.company;
 
 /**
- * Created by lilei on 2017/6/10.
+ *
+ * @author lilei
+ * @date 2017/6/10
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.printf("Hello World!");
+        System.out.print("Hello World!");
     }
 }
