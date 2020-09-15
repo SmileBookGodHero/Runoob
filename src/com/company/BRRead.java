7e99200c9aa44abf732794b9c8e082f2b3da54aa
李磊 <1198189741@qq.com>
2020-09-15 18:33:34 +0800

    修改包名称，使用java1.7版本jdk，不使用外部jar

---
+++ src/com/company/BRRead.java
@@ -0,0 +1,22 @@
+package com.company;
+
+/**
+ * Created by lilei on 2017/6/29.
+ */
+
+import java.io.*;
+
+public class BRRead {
+    public static void main(String args[]) throws IOException {
+        char c;
+        // 使用 System.in 创建 BufferedReader
+        BufferedReader br = new BufferedReader(new
+                InputStreamReader(System.in));
+        System.out.println("输入字符, 按下 'q' 键退出。");
+        // 读取字符
+        do {
+            c = (char) br.read();
+            System.out.println(c);
+        } while (c != 'q');
+    }
+}
---
+++ src/com/company/datastructure/Dictionary.java
@@ -0,0 +1,10 @@
+package com.company.datastructure;
+
+/**
+ * Created by lilei on 2017/7/4.
+ * Dictionary 类是一个抽象类，用来存储键/值对，作用和Map类相似。
+ * 给出键和值，你就可以将值存储在Dictionary对象中。
+ * 一旦该值被存储，就可以通过它的键来获取它，所以和Map一样，Dictionary也可以作为一个键值列表。
+ */
+public class Dictionary {
+}
