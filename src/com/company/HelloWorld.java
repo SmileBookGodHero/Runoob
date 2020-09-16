80716473f02f564435484c361149faf0b0153fbc
李磊 <1198189741@qq.com>
2020-09-16 15:17:01 +0800

    代码更新

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * @author lilei
+ * @date 2020-09-16 14:53
+ * @apiNote
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/com/company/aaa/InitializationDemo.java
@@ -0,0 +1,44 @@
+package com.company.aaa;
+
+/**
+ * @author lilei
+ * @date 2020-09-16 15:07
+ * @apiNote
+ */
+
+public class InitializationDemo {
+    public static void main(String[] args) {
+        System.out.println("爸爸的岁数:" + Son.factor);
+    }
+}
+
+class Grandpa {
+    static {
+        System.out.println("爷爷在静态代码块");
+    }
+}
+
+class Father extends Grandpa {
+    public static int factor = 25;
+
+    static {
+        System.out.println("爸爸在静态代码块");
+    }
+
+    public Father() {
+        System.out.println("我是爸爸~");
+    }
+}
+
+class Son extends Father {
+    static {
+        System.out.println("儿子在静态代码块");
+    }
+
+    public Son() {
+        System.out.println("我是儿子~");
+    }
+}
+
+
+
